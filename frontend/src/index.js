13cf74b1b260ba46f23df27da4b170086a059153
IzzetCanSen <izzet.sen@student.ehb.be>
2023-01-20 10:03:57 +0100

    Added hamburger navigation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,17 @@
-console.log("test");
+window.onload = function () {
+  const hamburgerNav = document.getElementById("hamburger-nav");
+  const hamburgerNavBtn = document.getElementById("hamburger-btn");
+  const hamburgerNavCloseBtn = document.getElementById("hamburger-close-btn");
+
+  hamburgerNavBtn.addEventListener("click", () => {
+    hamburgerNav.style.display = "flex";
+    hamburgerNavBtn.style.display = "none";
+    hamburgerNavCloseBtn.style.display = "flex";
+  });
+
+  hamburgerNavCloseBtn.addEventListener("click", () => {
+    hamburgerNav.style.display = "none";
+    hamburgerNavBtn.style.display = "flex";
+    hamburgerNavCloseBtn.style.display = "none";
+  });
+};
