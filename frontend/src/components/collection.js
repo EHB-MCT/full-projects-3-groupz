1150859126736c4aad828e95d72e27c32cafa626
IzzetCanSen <izzet.sen@student.ehb.be>
2023-01-24 10:08:03 +0100

    Deleted window.onload() & linked pages

--- frontend/src/components/collection.js
+++ frontend/src/components/collection.js
@@ -2,43 +2,42 @@ import nav from "../utils/nav.js";
 import { getArtworkData } from "../utils/getData.js";
 import filter from "../utils/filter.js";
 
-window.onload = function () {
-  nav();
+nav();
 
-  const filterBtn = document.getElementById("filter-btn-container");
-  const filterCloseBtn = document.getElementById("filter-close-btn");
-  const filterPopupBg = document.getElementById("filter-popup-bg");
-  const filterPopup = document.getElementById("filter-popup");
-  filter(filterBtn, filterCloseBtn, filterPopupBg, filterPopup);
+const filterBtn = document.getElementById("filter-btn-container");
+const filterCloseBtn = document.getElementById("filter-close-btn");
+const filterPopupBg = document.getElementById("filter-popup-bg");
+const filterPopup = document.getElementById("filter-popup");
+filter(filterBtn, filterCloseBtn, filterPopupBg, filterPopup);
 
-  const collectionC1 = document.getElementById("collection-column1");
-  const collectionC2 = document.getElementById("collection-column2");
-  const collectionC3 = document.getElementById("collection-column3");
+const collectionC1 = document.getElementById("collection-column1");
+const collectionC2 = document.getElementById("collection-column2");
+const collectionC3 = document.getElementById("collection-column3");
 
-  let c1Counter = 0;
-  let c2Counter = 0;
-  let c3Counter = 0;
+let c1Counter = 0;
+let c2Counter = 0;
+let c3Counter = 0;
 
-  let c1String = "";
-  let c2String = "";
-  let c3String = "";
+let c1String = "";
+let c2String = "";
+let c3String = "";
 
-  getArtworkData().then((response) => {
-    response.forEach((artwork) => {
-      if (c1Counter <= c2Counter && c1Counter <= c3Counter) {
-        c1String += `<img src="${artwork.img}" />`;
-        c1Counter++;
-      } else if (c2Counter <= c3Counter && c2Counter <= c1Counter) {
-        c2String += `<img src="${artwork.img}" />`;
-        c2Counter++;
-      } else if (c3Counter <= c1Counter && c3Counter <= c2Counter) {
-        c3String += `<img src="${artwork.img}" />`;
-        c3Counter++;
-      }
-    });
-
-    collectionC1.innerHTML = c1String;
-    collectionC2.innerHTML = c2String;
-    collectionC3.innerHTML = c3String;
+getArtworkData().then((response) => {
+  console.log(response);
+  response.forEach((artwork) => {
+    if (c1Counter <= c2Counter && c1Counter <= c3Counter) {
+      c1String += `<img src="${artwork.img}" />`;
+      c1Counter++;
+    } else if (c2Counter <= c3Counter && c2Counter <= c1Counter) {
+      c2String += `<img src="${artwork.img}" />`;
+      c2Counter++;
+    } else if (c3Counter <= c1Counter && c3Counter <= c2Counter) {
+      c3String += `<img src="${artwork.img}" />`;
+      c3Counter++;
+    }
   });
-};
+
+  collectionC1.innerHTML = c1String;
+  collectionC2.innerHTML = c2String;
+  collectionC3.innerHTML = c3String;
+});
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,36 +1,3 @@
 import nav from "./utils/nav.js";
 
-window.onload = function () {
-  nav();
-};
-
-//fetch API
-// async function getData() {
-//   let response = await fetch("http://localhost:6456/art");
-//   if (response.status == 200) {
-//     return await response.json();
-//   }
-// }
-
-// getData().then((response) =>
-//   response.forEach((item) => {
-//     const Titel = item.Titel;
-//     const Kunstenaar = item.Kunstenaar;
-//     const Verkoopprijs = item.Verkoopprijs;
-//     const breedte = item.breedte;
-//     const hoogte = item.hoogte;
-//     const diepte = item.diepte;
-//     const Archief = item.Archief;
-//     const Hoofdkleuren = item.Hoofdkleuren;
-//     const categories = item.categories;
-//     const Locatie = item.Locatie;
-//     const img = item.img;
-
-//     let htmlString = "";
-//     htmlString += `<div>
-
-//   </div>`;
-
-//     document.getElementById("").innerHTML += htmlString;
-//   })
-// );
+nav();
