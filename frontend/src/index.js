13b9f9378313f13ecc9775466bad5c16c7b22c49
ALEXANDERMERSSEMAN <alexander.mersseman2003@gmail.com>
2023-01-24 10:02:42 +0100

    Login page aangemaakt

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,33 +4,35 @@ window.onload = function () {
   nav();
 };
 
-//fetch API
-// async function getData() {
-//   let response = await fetch("http://localhost:6456/art");
-//   if (response.status == 200) {
-//     return await response.json();
-//   }
-// }
+fetch API
+async function getData() {
+  let response = await fetch("http://localhost:6456/art");
+  if (response.status == 200) {
+    return await response.json();
+  }
+}
 
-// getData().then((response) =>
-//   response.forEach((item) => {
-//     const Titel = item.Titel;
-//     const Kunstenaar = item.Kunstenaar;
-//     const Verkoopprijs = item.Verkoopprijs;
-//     const breedte = item.breedte;
-//     const hoogte = item.hoogte;
-//     const diepte = item.diepte;
-//     const Archief = item.Archief;
-//     const Hoofdkleuren = item.Hoofdkleuren;
-//     const categories = item.categories;
-//     const Locatie = item.Locatie;
-//     const img = item.img;
+getData().then((response) =>
+  response.forEach((item) => {
+    const Titel = item.Titel;
+    const Kunstenaar = item.Kunstenaar;
+    const Verkoopprijs = item.Verkoopprijs;
+    const breedte = item.breedte;
+    const hoogte = item.hoogte;
+    const diepte = item.diepte;
+    const Archief = item.Archief;
+    const Hoofdkleuren = item.Hoofdkleuren;
+    const categories = item.categories;
+    const Locatie = item.Locatie;
+    const img = item.img;
 
-//     let htmlString = "";
-//     htmlString += `<div>
+    let htmlString = "";
+    htmlString += `<div>
 
-//   </div>`;
+  </div>`;
 
-//     document.getElementById("").innerHTML += htmlString;
-//   })
-// );
+    document.getElementById("").innerHTML += htmlString;
+  })
+);
+  document.getElementById("").innerHTML += htmlString;
+}));
