27e42717891089ce12607be224ed3a90a878efec
ALEXANDERMERSSEMAN <alexander.mersseman2003@gmail.com>
2023-01-26 15:41:27 +0100

    login WIP

--- frontend/src/utils/login.js
+++ frontend/src/utils/login.js
@@ -1,24 +1,31 @@
 document.getElementById('loginform').addEventListener("submit", event => {
-    event.preventDefault()
+  event.preventDefault()
 
-    let user = {}
-    user.email = document.getElementById("inputEmail").value;
-    user.password = document.getElementById("inputPassword").value;
+  let user = {}
+  user.email = document.getElementById("inputEmail").value;
+  user.password = document.getElementById("inputPassword").value;
 
-    getArtworkData("http://localhost:6456/login", "POST", user).then(result => {
-      document.getElementById('welcomemessage').innerText = `${user.username}`
-      document.getElementById('succesmessageinlog').innerHTML = `Je bent succesvol ingelogd!`
-      sessionStorage.setItem('user', JSON.stringify(result.data))
-    })
+  getArtworkData("http://localhost:6456/login", "POST", user).then(result => {
+    sessionStorage.setItem('user', JSON.stringify(result.data))
   })
+})
 
-  async function getArtworkData(url, method, data){
-    let resp = await fetch(url, {
-      method: method,
-      headers: {
-        'Content-Type': "application/json"
-      },
-      body: JSON.stringify(data)
-    });
-    return await resp.json();
-  }
+async function getArtworkData(url, method, data){
+  let resp = await fetch(url, {
+    method: method,
+    headers: {
+      'Content-Type': "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+  return await resp.json();
+}
+
+let user = JSON.parse(sessionStorage.getItem('user'))
+
+if(user){
+  document.getElementById('welcomemessage').innerText = `${user.username}`
+  document.getElementById('succesmessageinlog').innerHTML = `Je bent succesvol ingelogd!`
+}else{
+  document.getElementById('succesmessageinlog').innerHTML = `Je bent succesvol ingelogd!`
+}
