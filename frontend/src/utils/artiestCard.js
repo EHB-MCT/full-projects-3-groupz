91e246e53c1c4f281adc9a7a8d7bda4648a1de90
ALEXANDERMERSSEMAN <alexander.mersseman2003@gmail.com>
2023-01-31 14:19:46 +0100

    Artiesten WIP

--- frontend/src/utils/artiestCard.js
+++ frontend/src/utils/artiestCard.js
@@ -13,9 +13,14 @@ export default function artiestCard(data) {
       <p>${item.discription}</p>
     </div>
     <div id="nav-btns-container" class="artiesten-button">
-    <a href=" ${item.website}"><button>Website</button></a>
-    <a href=""><button>meer info</button></a>
+    <a href="${item.website}"><button>Website</button></a>
+    <a href=""><button onclick="artiestInfo()">Meer info</button></a>
   </div>`;
     })
     artisten.innerHTML = htmlString
+}
+
+function artiestInfo(){
+  console.log("yes");
+  //const infobtn = document.getElementById("meerinfo");
 }
