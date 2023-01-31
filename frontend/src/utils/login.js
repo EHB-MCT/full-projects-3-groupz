66dfc761e78fa37434a3fb2e57dc6aae8d478e50
Wail El Majdoubi <91526221+Wail-ELM@users.noreply.github.com>
2023-01-31 14:05:52 +0100

    nav fixed

--- frontend/src/utils/login.js
+++ frontend/src/utils/login.js
@@ -4,20 +4,34 @@ document.getElementById('loginform').addEventListener("submit", event => {
   let user = {}
   user.email = document.getElementById("inputEmail").value;
   user.password = document.getElementById("inputPassword").value;
-  
+
   //let user = JSON.parse(sessionStorage.getItem('user'))
   getArtworkData("http://localhost:6456/login", "POST", user).then(result => {
     sessionStorage.setItem('user', JSON.stringify(result.data))
-    if(user){
+    if (user) {
       document.getElementById('welcomemessage').innerText = `${result.data.username}`
       document.getElementById('succesmessageinlog').innerHTML = `Je bent succesvol ingelogd!`
-    }else{
+    } else {
       document.getElementById('succesmessageinlog').innerHTML = `Kijk of al je velden correct zijn ingevuld of registreer eerst!`
     }
+
+
+    if (user) {
+      document.getElementById('favorite_button').addEventListener("click", () => {
+        window.location = "./favorieten.html"
+      }) = `${result.data.username}`
+
+    } else {
+      document.getElementById('text_fav').innerHTML = `Maak eerst een account aan!`
+    }
   })
+
+
+
+
 })
 
-async function getArtworkData(url, method, data){
+async function getArtworkData(url, method, data) {
   let resp = await fetch(url, {
     method: method,
     headers: {
